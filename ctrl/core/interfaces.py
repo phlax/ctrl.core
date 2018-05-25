819d75df94bcd749e055b484ee294972949e710c
Ryan Northey <ryan@synca.io>
2018-05-25 23:05:31 +0100

    Move interfaces to ctrl.core

--- ctrl/core/interfaces.py
+++ ctrl/core/interfaces.py
@@ -18,3 +18,63 @@ class IListener(Interface):
 
     def listen():
         pass
+
+
+class ISystemctl(Interface):
+
+    async def start(service):
+        pass
+
+    async def stop(service):
+        pass
+
+    async def status(service):
+        pass
+
+
+class IServerConfig(Interface):
+
+    pass
+
+
+class ICtrlConfig(Interface):
+
+    def read():
+        pass
+
+    def write():
+        pass
+
+
+class IConfiguration(Interface):
+
+    def configure():
+        pass
+
+
+class ICommandable(Interface):
+
+    def handle(*args):
+        pass
+
+
+class ICommand(Interface):
+
+    def handle(*args):
+        pass
+
+
+class ISubcommand(Interface):
+
+    def handle(*args):
+        pass
+
+
+class ICommandRunner(Interface):
+
+    def handle(*args):
+        pass
+
+
+class IShell(Interface):
+    pass
